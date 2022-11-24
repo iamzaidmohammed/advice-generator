289ccf3548dc425a2deb3274b747ce33048e7b3c
Zaid Mohammed Zainudeen <zainudeenzaidmohammed7@gmail.com>
2022-11-24 06:20:11 +0000

    created the advice generator component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import divider from "./images/pattern-divider-mobile.svg";
+import dice from "./images/icon-dice.svg";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [advice, setAdvice] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.adviceslip.com/advice")
+      .then((res) => res.json())
+      .then((slip) => {
+        setAdvice(slip.slip);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  const handleFetchButton = () => {
+    fetch("https://api.adviceslip.com/advice")
+      .then((res) => res.json())
+      .then((slip) => {
+        setAdvice(slip.slip);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="w-screen h-screen mx-auto relative bg-DarkBlue">
+      <div className="w-72 h-fit mx-auto absolute top-2/4 left-2/4 -translate-x-1/2 -translate-y-1/2 bg-DarkGrayishBlue py-8 px-5 rounded-lg shadow-lg">
+        <p className="text-NeonGreen uppercase text-xs text-center pt-1 mb-5">
+          Advice #{advice.id}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <h2 className="text-gray-100 text-center ">"{advice.advice}"</h2>
+
+        <img src={divider} alt="" className="py-5" />
+
+        <button className="bg-NeonGreen w-10 h-10 absolute -bottom-10 left-1/2 -translate-x-1/2 -translate-y-1/2 flex items-center justify-center rounded-full">
+          <img onClick={handleFetchButton} src={dice} alt="" />
+        </button>
+      </div>
     </div>
   );
 }
