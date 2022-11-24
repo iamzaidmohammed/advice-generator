4b277debe7fbbcfe5ceb71fda9dfcaa4e15000f5
Zaid Mohammed Zainudeen <zainudeenzaidmohammed7@gmail.com>
2022-11-24 07:36:06 +0000

    Added responsiveness and updated README

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
-import divider from "./images/pattern-divider-mobile.svg";
+import mobiledivider from "./images/pattern-divider-mobile.svg";
+import desktopdivider from "./images/pattern-divider-desktop.svg";
 import dice from "./images/icon-dice.svg";
 import { useEffect, useState } from "react";
 
@@ -30,17 +31,18 @@ function App() {
 
   return (
     <div className="w-screen h-screen mx-auto relative bg-DarkBlue">
-      <div className="w-72 h-fit mx-auto absolute top-2/4 left-2/4 -translate-x-1/2 -translate-y-1/2 bg-DarkGrayishBlue py-8 px-5 rounded-lg shadow-lg">
+      <div className="w-72 md:w-96 h-fit mx-auto absolute top-2/4 left-2/4 -translate-x-1/2 -translate-y-1/2 bg-DarkGrayishBlue py-8 px-5 rounded-lg shadow-lg">
         <p className="text-NeonGreen uppercase text-xs text-center pt-1 mb-5">
           Advice #{advice.id}
         </p>
 
         <h2 className="text-gray-100 text-center ">"{advice.advice}"</h2>
 
-        <img src={divider} alt="" className="py-5" />
+        <img src={mobiledivider} alt="" className="py-5 md:hidden" />
+        <img src={desktopdivider} alt="" className="py-5 hidden md:block" />
 
-        <button className="bg-NeonGreen w-10 h-10 absolute -bottom-10 left-1/2 -translate-x-1/2 -translate-y-1/2 flex items-center justify-center rounded-full">
-          <img onClick={handleFetchButton} src={dice} alt="" />
+        <button className="bg-NeonGreen w-10 h-10 absolute -bottom-10 left-1/2 -translate-x-1/2 -translate-y-1/2 flex items-center justify-center rounded-full transition-shadow">
+          <img onClick={handleFetchButton} src={dice} alt="dice" />
         </button>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,7 @@ module.exports = {
         Manrope: ["Manrope", "sans-serif"],
       },
       colors: {
-        LightCyan: "hsl(193, 38%, 86%)",
         NeonGreen: "hsl(150, 100%, 66%)",
-        GrayishBlue: "hsl(217, 19%, 38%)",
         DarkGrayishBlue: "hsl(217, 19%, 24%)",
         DarkBlue: "hsl(218, 23%, 16%)",
       },
